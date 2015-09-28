86b568ac8da0c1789c1d12ce5624173370bd2d5d
Thomas <thomaszhangswe@gmail.com>
2015-09-28 21:38:53 +0200

    Update main.R

--- R/main.R
+++ R/main.R
@@ -1,23 +1,7 @@
-library(httr)
+rm(list=ls())
 
-r <- GET(paste("http://data.goteborg.se/AirQualityService/v1.0/",
-         "LatestMeasurement/1cbbaf67-3671-41b3-b31c-3d735afae826?format=Json",sep=""))
+#library(httr)
 
-http_status(r)
-#req <-GET(paste("http://data.goteborg.se/TravelTimesService/v1.0/",
-          #"LatestTravelTimes/c4fdabd6-5e3a-4a54-b38d-e312d2740395?format=Json",sep=""))
-
-#reqq <-GET(paste("http://data.goteborg.se/TravelTimesService/v1.0/",
-           #"Routes/c4fdabd6-5e3a-4a54-b38d-e312d2740395?format=Json",sep=""))
-
-cont <- content(r)
-charvec <- unlist(cont)
-starttime <- epochPOSIX(charvec[1])
-stoptime <- epochPOSIX(charvec[2])
-
-tofile <- data.frame(starttime,stoptime)
-
-#setwd("D:/R_HW/Shinyapp/Newapp/www")
-write.csv(tofile, file = "time.csv", row.names = FALSE)
-read <- read.csv(file = "time.csv")
-#setwd("D:/R_HW/Lab5")
+channels <- get_channels()
+programs <- get_programs(channels)
+song <- get_song("P2",channels)
